2115521b1cde45f0389acaa21ebdf62854b81b36
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-10-19 22:23:08 -0300

    Create BMquestao5.java

---
+++ Atividade 4.03/BMquestao5.java
@@ -0,0 +1,20 @@
+importar  java . útil . Scanner ;
+ classe  pública QUESTAO5 {
+    public  static  void  main ( String [] args ){
+      int  numero ;
+       Entrada do Scanner = novo  Scanner ( System . in );
+
+      Sistema . fora . println ( "Escola um numero de 1 a 7:" );
+      numero = entrada . nextInt ();
+
+      interruptor ( numero ) {
+        caso  1 : Sistema . fora . println ( "Domingo" ); quebrar ;
+        caso  2 : Sistema . fora . println ( "Segunda-feira" ); quebrar ;
+        caso  3 : Sistema . fora . println ( "Terça-feira" ); quebrar ;
+        caso  4 : Sistema . fora . println ( "Quarta-feira" ); quebrar ;
+        caso  5 : Sistema . fora . println ( "Quinta-feira" ); quebrar ;
+        caso  6 : Sistema . fora . println ( "Sexta-feira" ); quebrar ;
+        caso  7 : Sistema . fora . println ( "Sabado" ); quebrar ;  
+      }
+    }
+}
